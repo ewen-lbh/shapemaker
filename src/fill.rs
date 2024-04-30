fe91750239dd38c0855f40dddc8b06f692d1f667
Ewen Le Bihan <hey@ewen.works>
2024-05-01 01:35:36 +0200

    🚧 Start implementing hatched fill patterns

--- src/fill.rs
+++ src/fill.rs
@@ -1,11 +1,53 @@
 use crate::{Color, ColorMapping, RenderCSS};
 
 #[derive(Debug, Clone, Copy)]
+pub enum HatchDirection {
+    Horizontal,
+    Vertical,
+    BottomUpDiagonal,
+    TopDownDiagonal,
+}
+
+impl HatchDirection {
+    pub fn svg_filter_name(&self) -> String {
+        "hatch-".to_owned()
+            + match self {
+                HatchDirection::Horizontal => "horizontal",
+                HatchDirection::Vertical => "vertical",
+                HatchDirection::BottomUpDiagonal => "bottom-up",
+                HatchDirection::TopDownDiagonal => "top-down",
+            }
+    }
+
+    pub fn svg_pattern_definition(&self) -> String {
+        // https://stackoverflow.com/a/14500054/9943464
+        format!(
+            r#"<pattern id="{}" patternUnits="userSpaceOnUse" width="{}" height="{}">"#,
+            self.svg_filter_name(),
+            todo!(),
+            todo!()
+        ) + &match self {
+            HatchDirection::BottomUpDiagonal => format!(
+                r#"<path 
+                    d="M-1,1 l2,-2
+                       M0,4 l4,-4
+                       M3,5 l2,-2" 
+                    style="stroke:black; stroke-width:1" 
+                />"#
+            ),
+            HatchDirection::Horizontal => todo!(),
+            HatchDirection::Vertical => todo!(),
+            HatchDirection::TopDownDiagonal => todo!(),
+        } + "</pattern>"
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub enum Fill {
     Solid(Color),
     Translucent(Color, f32),
-    Hatched,
-    Dotted,
+    Hatched(HatchDirection),
+    Dotted(f32),
 }
 
 impl RenderCSS for Fill {
@@ -17,8 +59,10 @@ impl RenderCSS for Fill {
             Fill::Translucent(color, opacity) => {
                 format!("fill: {}; opacity: {};", color.to_string(colormap), opacity)
             }
-            Fill::Dotted => unimplemented!(),
-            Fill::Hatched => unimplemented!(),
+            Fill::Dotted(radius) => unimplemented!(),
+            Fill::Hatched(direction) => {
+                format!("fill: url(#{});", direction.svg_filter_name())
+            }
         }
     }
 
